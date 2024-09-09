e531f331dab330a434f687087517a2b7bb8a7024
xzxzzz <1254345012@qq.com>
2024-09-09 21:52:53 +0800

    更正YOLO的训练轮数

--- TrainModel/trainYOLO.py
+++ TrainModel/trainYOLO.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
 model = YOLO('models/yolov5su.pt')
-model.train(data="tongue.yaml", epochs=1, imgsz=640)
+model.train(data="tongue.yaml", epochs=15, imgsz=640)
 # model.val(data="tongue.yaml")
